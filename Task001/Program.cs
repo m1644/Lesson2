5f2369240bca87f1a9d5a90896f5053f368c1530
MaxX <maxx-f@mail.ru>
2022-08-09 10:32:15 +0300

    Add Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100); // [10, 99] == (10, 100)
+Console.WriteLine(number);
+int digit1 = number / 10; // 74 / 10 = 7 (int)
+int digit2 = number % 10; // 74 % 10 = 4
+if (digit1 > digit2)
+    Console.WriteLine(digit1);
+else
+    Console.WriteLine(digit2);
